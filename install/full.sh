cb94266a8145a769a2bd90c8b2d141ac2277daa5
OliverMount <oliverjmount@yahoo.com>
2025-05-08 15:54:40 +0900

    updated

---
+++ install/full.sh
@@ -0,0 +1,16 @@
+# Define base and subdirectories
+appDIR=$HOME
+defDIR="$appDIR/def_files"
+imageDIR="$appDIR/images"
+tmpDIR="$appDIR/tmp"
+cacheDIR="$appDIR/cache"
+
+# Export for Apptainer
+export APPTAINER_TMPDIR="$tmpDIR"
+export APPTAINER_CACHEDIR="$cacheDIR"
+
+# Build the sandbox image (cn for CoreNEURON)
+sudo apptainer build --fix-perms --sandbox "$imageDIR/cn_sandbox" "$defDIR/cn.def"
+
+# After building convert to sif
+sudo apptainer build --fix-perms "$imageDIR/cn.sif" "$imageDIR/cn_sandbox"
